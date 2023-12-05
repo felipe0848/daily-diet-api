57622291eb0f8c27a7f47b07d87698962fdab9e1
Felipe Soares <felipe.11.11@hotmail.com>
2023-12-05 11:09:20 -0400

    Feat: setup enviroment variables

---
+++ src/env/index.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod'
+import { config } from 'dotenv'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('invalid enviroment variables', _env.error.format())
+
+  throw new Error('invalid enviroment variables')
+}
+
+export const env = _env.data
